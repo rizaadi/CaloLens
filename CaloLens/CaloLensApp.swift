147eba1f44ccdfa3bb687f44e4324cdbd74f005b
rizaadi <rizaadi890@gmail.com>
2024-03-01 14:14:00 +0700

    Initial Commit

---
+++ CaloLens/CaloLensApp.swift
@@ -0,0 +1,17 @@
+//
+//  CaloLensApp.swift
+//  CaloLens
+//
+//  Created by Riza Adi Kurniawan on 01/03/24.
+//
+
+import SwiftUI
+
+@main
+struct CaloLensApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CaloLens/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CaloLens
+//
+//  Created by Riza Adi Kurniawan on 01/03/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
